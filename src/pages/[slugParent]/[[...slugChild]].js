ac4b34222922131163375796fa520785e87484d1
Jacopo Riccioni <EsimioJ@users.noreply.github.com>
2022-07-26 19:56:10 +0200

    Update [[...slugChild]].js
    modifica da https://discord.com/channels/758750550057287711/758750550057287715/1001493160767266898

--- src/pages/[slugParent]/[[...slugChild]].js
+++ src/pages/[slugParent]/[[...slugChild]].js
@@ -162,7 +162,7 @@ export async function getStaticPaths() {
     });
 
   return {
-    paths,
+    paths: paths.filter(({ params }) => typeof param.slugParent === 'string'),
     fallback: false,
   };
 }
